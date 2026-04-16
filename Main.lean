3b989d4a2a0fdcb31f4d4817affb71f2b36a620f
Marek Petrik <mpetrik@cs.unh.edu>
2026-04-16 12:18:54 -0400

    updated the proof

--- Main.lean
+++ Main.lean
@@ -3,12 +3,17 @@ import MDPLib.Risk.VaR
 
 theorem fancy : True := True.intro
 
+example : 0 ≤ (0.5 : ℚ) := by norm_num
+
 def main : IO Unit := -- main
   let x : Fin 3 → ℚ := ![1,2,3]
   let p : Fin 3 → ℚ := ![0.1, 0.5, 0.4]
+  set_option diagnostics true in 
   let P : Findist 3 := 
-    {p := p, prob := by  simp [dotProduct, p]; sorry; , nneg := by sorry }     
-  let α : Risk.RiskLevel := ⟨0.5, by unfold Risk.IsRiskLevel; constructor; linarith; linarith  ⟩
+    { p := p, 
+      prob := by simp [dotProduct, p]; decide_cbv, 
+      nneg := (fun ω => by fin_cases ω; repeat (simp [p]; norm_num))}     
+  let α : Risk.RiskLevel := ⟨0.5, by constructor; linarith; linarith⟩
 
   let v := Risk.FinVaR P x α
 
